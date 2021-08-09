1c43565ed8be9d3c143d9781fc01342e0eeb78df
Mr. Unknown <noom1009@gmail.com>
2021-08-09 22:09:34 +0700

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,40 +2,63 @@
   <div class="home">
     <div class="content">
       <Navbar />
-      <br>
-      <h3 class="row gx-3 gy-2 align-items-center"><center>ลอตเตอรี่ออนไลน์</center></h3>
-      <p class="row gx-3 gy-2 align-items-center text-danger"><center>ลอตเตอรี่งวดวันที่ {{ deadlinedate }}</center></p>
+      <br />
+      <h3 class="row gx-3 gy-2 align-items-center">
+        <center>ลอตเตอรี่ออนไลน์</center>
+      </h3>
+      <p class="row gx-3 gy-2 align-items-center text-danger">
+        <center>ลอตเตอรี่งวดวันที่ {{ deadlinedate }}</center>
+      </p>
       <h3 class="row gx-3 gy-2 align-items-center">
         <center>Comming Soon: {{ deadlinedate }}</center>
       </h3>
       <flip-countdown :deadline="deadline1ts"></flip-countdown>
       <Lotto />
- <section class="products">
-    <div v-for="product in products" :key="product.id" class="product">
-      <h3 class="product__header">{{ product.name }}</h3>
-      <img
-        src="https://via.placeholder.com/150"
-        :alt="product.name"
-        class="product__image"
-      >
-      <p class="product__description">{{ product.description }}</p>
-      <div class="cart">
-        <button
-          @click="updateCart(product, 'subtract')"
-          class="cart__button"
-        >
-          -
-        </button>
-        <span class="cart__quantity">{{ product.quantity }}</span>
-        <button
-          @click="updateCart(product, 'add')"
-          class="cart__button"
-        >
-          +
-        </button>
-      </div>
+      <section class="content">
+<div class="row">
+        <div v-for="lotto in info" :key="lotto.f_code" class="col-md-4">
+          <h3 class="product__header">{{ lotto.f_number }}</h3>
+          <img v-bind:src="'/' + lotto.f_pic" :alt="lotto.f_number" class="product_image"/> 
+          <p class="product__description">{{ lotto.f_barcode }}</p>
+          <div class="cart">
+            <button
+              @click="updateCart(lotto, 'subtract')"
+              class="cart__button"
+            >
+              -
+            </button>
+            <span class="cart__quantity">{{ lotto.f_price }}</span>
+            <button @click="updateCart(lotto, 'add')" class="cart__button">
+              +
+            </button>
+          </div>
+        </div>
+</div>
+      </section>
+      <section class="products">
+        <div v-for="product in products" :key="product.id" class="product">
+          <h3 class="product__header">{{ product.name }}</h3>
+          <img
+            src="https://via.placeholder.com/150"
+            :alt="product.name"
+            class="product__image"
+          />
+          <p class="product__description">{{ product.description }}</p>
+          <div class="cart">
+            <button
+              @click="updateCart(product, 'subtract')"
+              class="cart__button"
+            >
+              -
+            </button>
+            <span class="cart__quantity">{{ product.quantity }}</span>
+            <button @click="updateCart(product, 'add')" class="cart__button">
+              +
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
-  </section>  
     <h2 class="nav__header">Products</h2>
     <div class="nav__cart">
       <button @click="showCart = !showCart">
@@ -53,7 +76,7 @@
         </ul>
       </div>
     </div>
-    </div>
+
     <Footer />
   </div>
 </template>
@@ -63,10 +86,9 @@ import FlipCountdown from "vue2-flip-countdown";
 import Navbar from "../components/Navbar.vue";
 import Footer from "../components/Footer.vue";
 import Lotto from "../components/Lotto.vue";
-
+import axios from 'axios';
 import moment from "moment";
-//const fmt = "YYYY-MM-DD";
-const DASH_DMY = 'DD-MM-YYYY';
+const DASH_DMY = "DD-MM-YYYY";
 export default {
   name: "app",
   components: {
@@ -78,61 +100,67 @@ export default {
   data() {
     return {
       deadline1ts: "2021-10-01",
-      deadlinedate :"01-10-2021",
+      deadlinedate: "01-10-2021",
       showCart: false,
-     products: [
+      info: null,
+      products: [
         {
           id: 1,
-          name: 'Product 1',
-          description: 'This is an incredibly awesome product',
+          name: "Product 1",
+          description: "This is an incredibly awesome product",
           quantity: 0,
         },
         {
           id: 2,
-          name: 'Product 2',
-          description: 'This is an incredibly awesome product',
+          name: "Product 2",
+          description: "This is an incredibly awesome product",
           quantity: 0,
         },
         {
           id: 3,
-          name: 'Product 3',
-          description: 'This is an incredibly awesome product',
+          name: "Product 3",
+          description: "This is an incredibly awesome product",
           quantity: 0,
-        }
+        },
       ],
     };
   },
   computed: {
     deadline1: function () {
-      return moment(this.deadlinedate).format(DASH_DMY); 
+      return moment(this.deadlinedate).format(DASH_DMY);
     },
     cart() {
-      return this.products.filter(product => product.quantity > 0);
+      return this.products.filter((product) => product.quantity > 0);
     },
     totalQuantity() {
       return this.products.reduce(
         (total, product) => total + product.quantity,
         0
       );
-    }
     },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:9000/lotto")
+      .then((response) => (this.info = response.data.data));
+  },
   methods: {
     timeElapsedHandler: function () {},
-          updateCart(product, updateType) {      
-        for (let i = 0; i < this.products.length; i++) {
-          if (this.products[i].id === product.id) {
-            if (updateType === 'subtract') {
-              if (this.products[i].quantity !== 0) {
-                this.products[i].quantity--;
-              }
-            } else {
-              this.products[i].quantity++;
+    updateCart(product, updateType) {
+      for (let i = 0; i < this.products.length; i++) {
+        if (this.products[i].id === product.id) {
+          if (updateType === "subtract") {
+            if (this.products[i].quantity !== 0) {
+              this.products[i].quantity--;
             }
-            
-            break;
+          } else {
+            this.products[i].quantity++;
           }
+
+          break;
         }
       }
+    },
   },
 };
 </script>
@@ -143,36 +171,37 @@ export default {
   justify-content: center;
 }
 .description {
-      font-size: 1.3rem;
-      margin-top: 1rem;
-    }
+  font-size: 1.3rem;
+  margin-top: 1rem;
+}
 .cart {
   margin-top: 2rem;
   text-align: center;
 }
 .button {
-    background: lightblue;
-    border: 0;
-    color: white;
-    cursor: pointer;
-    font-size: 1.5rem;
-    font-weight: bold;
-    height: 2.5rem;
-    width: 2.5rem;
-  }
-  .quantity {
-    font-size: 1.5rem;
-    margin: 0 1rem;
-  }
-    .image {
-      display: block;
-      margin: 1rem auto;
-    }
-  .product {
-    border: 1px solid lightgray;
-    border-radius: 10px;
-    margin: 2rem;
-    padding: 1rem; 
-  }
+  background: lightblue;
+  border: 0;
+  color: white;
+  cursor: pointer;
+  font-size: 1.5rem;
+  font-weight: bold;
+  height: 2.5rem;
+  width: 2.5rem;
+}
+.quantity {
+  font-size: 1.5rem;
+  margin: 0 1rem;
+}
+.product_image {
+  display: block;
+  margin: 1rem auto;
+  width: 50%;
+  height: 50%;
+}
+.product {
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  margin: 2rem;
+  padding: 1rem;
+}
 </style>
-
